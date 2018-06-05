93dbdb15e2654a05b78cf737431800623ca207d2
Johan Nordberg <its@johan-nordberg.com>
2018-06-05 14:10:20 +0200

    Vendor secp256k1

---
+++ Sources/secp256k1/src/num_impl.h
@@ -0,0 +1,13 @@
+/**********************************************************************
+ * Copyright (c) 2013, 2014 Pieter Wuille                             *
+ * Distributed under the MIT software license, see the accompanying   *
+ * file COPYING or http://www.opensource.org/licenses/mit-license.php.*
+ **********************************************************************/
+
+#ifndef SECP256K1_NUM_IMPL_H
+#define SECP256K1_NUM_IMPL_H
+
+#include "num.h"
+#include "num_gmp_impl.h"
+
+#endif /* SECP256K1_NUM_IMPL_H */
