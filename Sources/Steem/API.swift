d7a166e2ab4d31b28db18055b2c45954f394cad1
Johan Nordberg <its@johan-nordberg.com>
2018-06-07 10:55:37 +0200

    Add GetAccounts API request

--- Sources/Steem/API.swift
+++ Sources/Steem/API.swift
@@ -62,4 +62,80 @@ public struct API {
             self.params = RequestParams([blockNum])
         }
     }
+
+    public struct Share: Decodable {
+        public let value: Int64
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.singleValueContainer()
+            if let intValue = try? container.decode(Int64.self) {
+                self.value = intValue
+            } else {
+                self.value = Int64(try container.decode(String.self)) ?? 0
+            }
+        }
+    }
+
+    /// The "extended" account object returned by get_accounts.
+    public struct ExtendedAccount: Decodable {
+        public let id: Int
+        public let name: String
+        public let owner: Authority
+        public let active: Authority
+        public let posting: Authority
+        public let memoKey: PublicKey
+        public let jsonMetadata: String
+        public let proxy: String
+        public let lastOwnerUpdate: Date
+        public let lastAccountUpdate: Date
+        public let created: Date
+        public let mined: Bool
+        public let recoveryAccount: String
+        public let resetAccount: String
+        public let lastAccountRecovery: Date
+        public let commentCount: UInt32
+        public let lifetimeVoteCount: UInt32
+        public let postCount: UInt32
+        public let canVote: Bool
+        public let votingPower: UInt16
+        public let lastVoteTime: Date
+        public let balance: Asset
+        public let savingsBalance: Asset
+        public let sbdBalance: Asset
+        public let sbdSeconds: String // uint128_t
+        public let sbdSecondsLastUpdate: Date
+        public let sbdLastInterestPayment: Date
+        public let savingsSbdBalance: Asset
+        public let savingsSbdSeconds: String // uint128_t
+        public let savingsSbdSecondsLastUpdate: Date
+        public let savingsSbdLastInterestPayment: Date
+        public let savingsWithdrawRequests: uint8_t
+        public let rewardSbdBalance: Asset
+        public let rewardSteemBalance: Asset
+        public let rewardVestingBalance: Asset
+        public let rewardVestingSteem: Asset
+        public let curationRewards: Share
+        public let postingRewards: Share
+        public let vestingShares: Asset
+        public let delegatedVestingShares: Asset
+        public let receivedVestingShares: Asset
+        public let vestingWithdrawRate: Asset
+        public let nextVestingWithdrawal: Date
+        public let withdrawn: Share
+        public let toWithdraw: Share
+        public let withdrawRoutes: UInt16
+        public let proxiedVsfVotes: [Share]
+        public let witnessesVotedFor: UInt16
+        public let lastPost: Date
+        public let lastRootPost: Date
+    }
+
+    /// Fetch accounts.
+    public struct GetAccounts: Request {
+        public typealias Response = [ExtendedAccount]
+        public let method = "get_accounts"
+        public let params: RequestParams<[String]>?
+        public init(names: [String]) {
+            self.params = RequestParams([names])
+        }
+    }
 }
